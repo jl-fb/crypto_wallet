9007eeb84b01f9b1930085248acb4c7440673c7f
jl-fb <jlborgh@gmail.com>
2019-08-20 23:15:58 -0300

    Fix I18n.l links para traduçao da pagina atual

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :mining_types
   get 'welcome/index'
   resources :coins
-  
+  get '/:locale' => 'welcome#index'
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "/:locale" do
+    resources :coins
+    resources :mining_types
+  end
 end
